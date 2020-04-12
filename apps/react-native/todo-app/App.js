e6e9361127a173a611134372cf941ec886cf53f0
pysix <prathapy67@gmail.com>
2020-04-12 21:29:47 +0530

    Revert "Add Todo App"
    This reverts commit 057d6c17d9088ab3c9e35cf326de8c57faddeb6e.

--- apps/react-native/todo-app/App.js
+++ apps/react-native/todo-app/App.js
@@ -1,72 +1,19 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import Header from './components/Header';
-import TodoItem from './components/TodoItem';
-import TodoForm from './components/TodoForm';
-import SandBox from './SandBox';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [todos, setTodo] = useState([
-    { text: 'Eat', key: "1" },
-    { text: 'Pubg', key: "2" },
-    { text: 'Repeat', key: "3" },
-  ])
-
-  const pressHandler = (id) => {
-    setTodo(prevTodos => {
-      return prevTodos.filter(item => item.key !== id);
-    })
-  };
-
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setTodo(prevTodos => {
-        return [
-          ...prevTodos,
-          { text: text, key: Math.random().toString() }
-        ]
-      })
-    } else {
-      Alert.alert('OOPS!', "Todo must be over 3 Chars long", [
-        { text: "Understood" }
-      ])
-    }
-  };
-
   return (
-    // <SandBox />
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <TodoForm submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({ item }) => (
-                <TodoItem
-                  onPress={pressHandler}
-                  item={item} />
-              )}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  content: {
-    flex: 1,
-    padding: 40
-  },
-  list: {
-    flex: 1,
-    marginTop: 20
-  }
 });
