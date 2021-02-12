54cb2e1a12606e9b5dff8eac68f8c044dc18fb5a
prathap <prathap@anakinmedia.com>
2021-02-12 23:39:14 +0530

    global-chat-responsive

--- apps/react-next-pwa/src/pages/global-chat.tsx
+++ apps/react-next-pwa/src/pages/global-chat.tsx
@@ -1,7 +1,8 @@
 import React, { Fragment, PureComponent } from 'react'
-import { Card, Row, Col, Typography, Button, Form, Input } from 'antd';
+import { Card, Row, Col, Typography, Button, Form, Input, Drawer } from 'antd';
 import Router from 'next/router';
 import io from 'socket.io-client';
+import Media from 'react-media';
 import MessageInput from '~/components/MessageInput';
 import MessageList from '~/components/MessageList';
 import { IMessageFormValues, IMessage } from '~/interfaces/props';
@@ -27,10 +28,11 @@ export default class index extends PureComponent<{}, IState> {
     }
 
     socket: any = React.createRef();
+    formRef: any = React.createRef();
 
     componentDidUpdate(_: {}, prevState: IState) {
         const { username } = this.state;
-        if (prevState.username !== username) {
+        if (username !== '' && prevState.username !== username) {
             this.socket = io(process.env.NEXT_PUBLIC_CHAT_SERVER_API || '', {
                 query: {
                     username
@@ -77,34 +79,54 @@ export default class index extends PureComponent<{}, IState> {
         }
     }
 
-    render() {
+    setChatUser = (name: string) => {
+        this.setState({ username: name });
+    };
+
+    handleUsernameSubmit = (values: any) => {
+        this.setChatUser(values.username);
+        this.formRef.current.resetFields();
+    };
+
+    onDrawerClose = () => {
+        this.socket = null;
+        this.setState({
+            messages: [],
+            username: ''
+        });
+        window.location.href = '/global-chat';
+    };
+
+    getUserNameInput = () => (
+        <Row className="messageInput">
+            <Form ref={this.formRef} onFinish={this.handleUsernameSubmit}>
+                <Row>
+                    <Col xs={18} sm={18} md={21} lg={21} xl={21}>
+                        <Form.Item
+                            name="username"
+                            rules={[{ required: true, message: 'username is required to continue chat' }]}
+                        >
+                            <Input placeholder="enter username to start chat" />
+                        </Form.Item>
+                    </Col>
+                    <Col xs={6} sm={6} md={3} lg={3} xl={3}>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">Chat</Button>
+                        </Form.Item>
+                    </Col>
+                </Row>
+            </Form>
+        </Row>
+    );
+
+    getChatBoxContent = (isMobile: boolean) => {
         const { messages, username } = this.state;
         return (
-            <div className="pageContainer">
-                <Card>
-                    <Row className="chatContainer">
-                        <Col xs={12} sm={12} md={10} lg={8} xl={8} className="headings">
-                            <Row
-                                onClick={() => Router.push('/')}
-                                style={{ cursor: 'pointer' }}
-                            >
-                                <Title>Chat</Title>
-                            </Row>
-                            <Row
-                                onClick={() => Router.push('/auth/login')}
-                                style={{ cursor: 'pointer' }}
-                            >
-                                <Title level={3}>Login</Title>
-                            </Row>
-                            <Row
-                                onClick={() => Router.push('/auth/signup')}
-                                style={{ cursor: 'pointer' }}
-                            >
-                                <Title level={3}>Sign Up</Title>
-                            </Row>
-                        </Col>
-                        <Col xs={12} sm={12} md={14} lg={16} xl={16} className="chatBox">
-                            {username && username !== '' ? (
+            <Col xs={24} sm={24} md={12} lg={16} xl={16}>
+                <div className="chatBoxContainer">
+                    <div className="chatBox">
+                        <Fragment>
+                            {username === '' && !isMobile ? this.getUserNameInput() : (
                                 <Fragment>
                                     <Row className="contactInfo">
                                         <Col>
@@ -114,35 +136,71 @@ export default class index extends PureComponent<{}, IState> {
                                     <MessageList messages={messages} socketId={username} chatType="global" />
                                     <MessageInput handleSendMessage={this.handleSendMessage} />
                                 </Fragment>
-                            ) : (
-                                    <Row className="messageInput">
-                                        <Form onFinish={(values) => {
-                                            this.setState({
-                                                username: values.username
-                                            })
-                                        }}>
-                                            <Row>
-                                                <Col span={21}>
-                                                    <Form.Item
-                                                        name="username"
-                                                        rules={[{ required: true, message: 'username is required to continue chat' }]}
-                                                    >
-                                                        <Input placeholder="enter username to start chat" />
-                                                    </Form.Item>
-                                                </Col>
-                                                <Col span={3}>
-                                                    <Form.Item>
-                                                        <Button type="primary" htmlType="submit">Chat</Button>
-                                                    </Form.Item>
-                                                </Col>
-                                            </Row>
-                                        </Form>
-                                    </Row>
-                                )}
-                        </Col>
-                    </Row>
-                </Card>
-            </div>
+                            )}
+                        </Fragment>
+                    </div>
+                </div>
+            </Col>
         )
+    }
+
+    render() {
+        const { username } = this.state;
+
+        return (
+            <Media query="(max-width: 599px)">
+                {(isMobile: boolean) => (
+                    <div className="pageContainer">
+                        <Card>
+                            <Row gutter={40} className="chatContainer">
+                                <Col xs={24} sm={24} md={12} lg={8} xl={8} className="headings">
+                                    <Row
+                                        onClick={() => Router.push('/')}
+                                        style={{ cursor: 'pointer' }}
+                                    >
+                                        <Title>Chat</Title>
+                                    </Row>
+                                    <Row
+                                        onClick={() => Router.push('/auth/login')}
+                                        style={{ cursor: 'pointer' }}
+                                    >
+                                        <Title level={3}>Login</Title>
+                                    </Row>
+                                    <Row
+                                        onClick={() => Router.push('/auth/signup')}
+                                        style={{ cursor: 'pointer' }}
+                                    >
+                                        <Title level={3}>Sign Up</Title>
+                                    </Row>
+                                    {isMobile && (
+                                        <div className="chatBoxContainer">
+                                            <div className="chatBox">
+                                                {this.getUserNameInput()}
+                                            </div>
+                                        </div>
+                                    )}
+                                </Col>
+                                {isMobile ? (
+                                    <Drawer
+                                        placement="bottom"
+                                        maskClosable
+                                        className="mobileChatDrawer"
+                                        visible={username !== ''}
+                                        onClose={this.onDrawerClose}
+                                    >
+                                        {this.getChatBoxContent(isMobile)}
+                                    </Drawer>
+                                ) : (
+                                        <Fragment>
+                                            {this.getChatBoxContent(isMobile)}
+                                        </Fragment>
+                                    )}
+                            </Row>
+                        </Card>
+                    </div>
+                )
+                }
+            </Media>
+        );
     }
 }
